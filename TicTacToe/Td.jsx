0e8ca9e365ba5af3bf2d3a7e7313337925f56674
jrucl2d <ysk789465@gmail.com>
2020-08-13 23:18:47 +0900

    TicTacToe using Context API done

--- TicTacToe/Td.jsx
+++ TicTacToe/Td.jsx
@@ -1,9 +1,10 @@
-import React, { memo, useCallback, useContext } from "react";
+import React, { memo, useCallback, useContext, useEffect } from "react";
 import { SET_CELL, TableContext } from "./TicTacToe";
 
 const Td = memo(({ rowIndex, colIndex }) => {
   // console.log("td rendered");
-  const { tableData, dispatch } = useContext(TableContext);
+  const { tableData, dispatch, winner } = useContext(TableContext);
+
   const onClickTd = useCallback(() => {
     if (tableData[rowIndex][colIndex]) {
       return;
--- TicTacToe/TicTacToe.jsx
+++ TicTacToe/TicTacToe.jsx
@@ -1,4 +1,4 @@
-import React, { useReducer, createContext, useMemo } from "react";
+import React, { useReducer, createContext, useMemo, useEffect } from "react";
 import Table from "./Table";
 
 const initialState = {
@@ -9,6 +9,7 @@ const initialState = {
   ],
   winner: "",
   turn: "O",
+  recentCell: [-1, -1],
 };
 
 // Context API
@@ -19,21 +20,45 @@ export const TableContext = createContext({
 
 // action
 export const SET_CELL = "SET_CELL";
+const CHANGE_TURN = "CHANGE_TURN";
+const WIN_GAME = "WIN_GAME";
+const RESET_GAME = "RESET_GAME";
 
 // reducer
 const reducer = (state, action) => {
   switch (action.type) {
     case SET_CELL: {
-      console.log(action.row, action.col);
       const tableData = [...state.tableData];
       tableData[action.row] = [...state.tableData[action.row]];
       tableData[action.row][action.col] = state.turn;
       return {
         ...state,
         tableData,
-        turn: state.turn === "O" ? "X" : "O",
+        recentCell: [action.row, action.col],
       };
     }
+    case CHANGE_TURN:
+      return {
+        ...state,
+        turn: state.turn === "O" ? "X" : "O",
+        winner: "",
+      };
+    case WIN_GAME:
+      return {
+        ...state,
+        winner: action.winner,
+      };
+    case RESET_GAME:
+      return {
+        ...state,
+        tableData: [
+          ["", "", ""],
+          ["", "", ""],
+          ["", "", ""],
+        ],
+        turn: "O",
+        recentCell: [-1, -1],
+      };
     default:
       return;
   }
@@ -41,9 +66,48 @@ const reducer = (state, action) => {
 
 const TicTacToe = () => {
   const [state, dispatch] = useReducer(reducer, initialState);
-  const { tableData, winner } = state;
+  const { tableData, winner, recentCell, turn } = state;
 
-  const value = useMemo(() => ({ tableData, dispatch }), [tableData]);
+  useEffect(() => {
+    const [row, col] = recentCell;
+    if (row < 0) {
+      return;
+    }
+    let win = false;
+    if (tableData[row][0] === turn && tableData[row][1] === turn && tableData[row][2] === turn) {
+      win = true;
+    }
+    if (tableData[0][col] === turn && tableData[1][col] === turn && tableData[2][col] === turn) {
+      win = true;
+    }
+    if (tableData[0][0] === turn && tableData[1][1] === turn && tableData[2][2] === turn) {
+      win = true;
+    }
+    if (tableData[0][2] === turn && tableData[1][1] === turn && tableData[2][0] === turn) {
+      win = true;
+    }
+    if (win) {
+      console.log(tableData);
+      dispatch({ type: WIN_GAME, winner: turn });
+      dispatch({ type: RESET_GAME });
+      return;
+    }
+    // 무승부
+    let done = true;
+    tableData.forEach((row) => {
+      row.forEach((col) => {
+        if (!col) done = false;
+      });
+    });
+    if (done) {
+      dispatch({ type: WIN_GAME, winner: "무승부잼이" });
+      dispatch({ type: RESET_GAME });
+      return;
+    }
+    dispatch({ type: CHANGE_TURN });
+  }, [tableData]);
+
+  const value = useMemo(() => ({ tableData, dispatch, winner }), [tableData]);
   return (
     <TableContext.Provider value={value}>
       <Table />
