e2f6e0de5830066105f0c5ae7a3c96268bcea381
jrucl2d <ysk789465@gmail.com>
2020-09-16 11:01:38 +0900

    rendering of undefined

--- react-skill/chapter2/src/App.js
+++ react-skill/chapter2/src/App.js
@@ -3,6 +3,11 @@ import React from "react";
 function App() {
   const name = "리액트쟁이";
   const number = 0;
+
+  const unde = undefined;
+  // return unde; // undefined를 리턴하면 오류남
+  // return unde || "값이 undefined입니다."; // or 연산자로 해결
+
   return (
     <>
       <h1>{name} 안녕!</h1>
@@ -15,6 +20,7 @@ function App() {
       {name === "리액트쟁이" && <h1>리액트쟁이</h1>}
       {name === "리액트돌이" && null}
       {number && <h1>영이다</h1>} {/* 이건 0이 출력됨 */}
+      {unde || "언디파인드"} {/* undefined를 렌더링하는 것은 가능 */}
     </>
   );
 }
