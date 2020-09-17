9b69bef14e487418a95a4d05aed66c883c4ae2af
jrucl2d <ysk789465@gmail.com>
2020-09-17 20:01:25 +0900

    Made New component

--- book/chapter2/src/App.js
+++ book/chapter2/src/App.js
@@ -1,12 +1,17 @@
 import React, { Component } from "react";
 import "./App.css";
+import MyComponent from "./MyComponent";
 
-class App extends Component {
-  render() {
-    const name = "react";
-    return <div className="react">{name} hi!</div>;
-  }
-}
+const App = () => {
+  return <MyComponent />;
+};
+
+// class App extends Component {
+//   render() {
+//     const name = "react";
+//     return <div className="react">{name} hi!</div>;
+//   }
+// }
 
 // function App() {
 //   const name = "리액트";
