2dadb1d17434a01afc11bfa2ed12a3784a92d5a9
jrucl2d <ysk789465@gmail.com>
2020-09-17 18:59:28 +0900

    using class component studied

--- book/chapter2/src/App.js
+++ book/chapter2/src/App.js
@@ -1,14 +1,21 @@
-import React from "react";
+import React, { Component } from "react";
 import "./App.css";
 
-function App() {
-  const name = "리액트";
-  return (
-    <>
-      <div className="react">{name}</div>
-      <input /> {/* self closing 태그 */}
-    </>
-  );
+class App extends Component {
+  render() {
+    const name = "react";
+    return <div className="react">{name} hi!</div>;
+  }
 }
+
+// function App() {
+//   const name = "리액트";
+//   return (
+//     <>
+//       <div className="react">{name}</div>
+//       <input /> {/* self closing 태그 */}
+//     </>
+//   );
+// }
 
 export default App;
