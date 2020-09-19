3ffbea0a5abb92998b3f13c32787a28884d83e56
jrucl2d <ysk789465@gmail.com>
2020-09-19 11:44:32 +0900

    Using arrow function to solve the this binding problem

--- book/chapter2/src/EventPractice.js
+++ book/chapter2/src/EventPractice.js
@@ -4,20 +4,32 @@ class EventPractice extends Component {
   state = {
     message: "",
   };
-  constructor(props) {
-    super(props);
-    this.handleChange = this.handleChange.bind(this); // DOM 이벤트로 등록되면서 메서드와 this의 관계가 끊어짐
-    this.handleClick = this.handleClick.bind(this); // 따라서 this를 바인딩해줘야 함. 안 그러면 undefined가 나옴
-  }
-  handleChange(e) {
+  //   constructor(props) {
+  //     super(props);
+  //     this.handleChange = this.handleChange.bind(this); // DOM 이벤트로 등록되면서 메서드와 this의 관계가 끊어짐
+  //     this.handleClick = this.handleClick.bind(this); // 따라서 this를 바인딩해줘야 함. 안 그러면 undefined가 나옴
+  //   }
+  //   handleChange(e) {
+  //     this.setState({
+  //       message: e.target.value,
+  //     });
+  //   }
+  //   handleClick() {
+  //     alert(this.state.message);
+  //     this.setState({ message: "" });
+  //   }
+
+  // 화살표 함수의 this를 사용하면 됨
+  handleChange = (e) => {
     this.setState({
       message: e.target.value,
     });
-  }
-  handleClick() {
+  };
+
+  handleClick = () => {
     alert(this.state.message);
     this.setState({ message: "" });
-  }
+  };
 
   render() {
     return (
