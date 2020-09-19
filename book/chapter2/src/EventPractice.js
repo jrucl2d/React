18b5d174dc285fcb14d0ddaf4197b3d83c05226e
jrucl2d <ysk789465@gmail.com>
2020-09-19 11:52:45 +0900

    class style component study done

--- book/chapter2/src/EventPractice.js
+++ book/chapter2/src/EventPractice.js
@@ -3,6 +3,7 @@ import React, { Component } from "react";
 class EventPractice extends Component {
   state = {
     message: "",
+    username: "",
   };
   //   constructor(props) {
   //     super(props);
@@ -22,13 +23,19 @@ class EventPractice extends Component {
   // 화살표 함수의 this를 사용하면 됨
   handleChange = (e) => {
     this.setState({
-      message: e.target.value,
+      [e.target.name]: e.target.value,
     });
   };
 
   handleClick = () => {
-    alert(this.state.message);
-    this.setState({ message: "" });
+    alert(this.state.username + ":" + this.state.message);
+    this.setState({ username: "", message: "" });
+  };
+
+  handleKeyPress = (e) => {
+    if (e.key === "Enter") {
+      this.handleClick();
+    }
   };
 
   render() {
@@ -37,10 +44,18 @@ class EventPractice extends Component {
         <h1>이벤트 연습</h1>
         <input
           type="text"
+          name="username"
+          placeholder="사용자명"
+          value={this.state.username}
+          onChange={this.handleChange}
+        />
+        <input
+          type="text"
           name="message"
           placeholder="아무거나 입력하시오"
           value={this.state.message}
           onChange={this.handleChange}
+          onKeyPress={this.handleKeyPress}
         />
         <button onClick={this.handleClick}>확인</button>
       </div>
