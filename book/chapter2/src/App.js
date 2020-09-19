181a9da885a63ba9b1fc2f0bd80bc477bbe4f06e
jrucl2d <ysk789465@gmail.com>
2020-09-19 11:42:05 +0900

    Basic event practice

--- book/chapter2/src/App.js
+++ book/chapter2/src/App.js
@@ -1,36 +1,8 @@
 import React from "react";
-import "./App.css";
-// import MyComponent from "./MyComponent";
-// import Counter from "./Counter";
-import Say from "./Say";
+import EventPractice from "./EventPractice";
 
 const App = () => {
-  // return <MyComponent name="React" />;
-  // return <MyComponent />; // defaultProps 값이 사용됨
-  return (
-    // <Counter />
-    <Say />
-    // <MyComponent name="으쟁이" favoriteNumber={3}>
-    //   리액트
-    // </MyComponent>
-  ); // props.children으로 표시
+  return <EventPractice />;
 };
-
-// class App extends Component {
-//   render() {
-//     const name = "react";
-//     return <div className="react">{name} hi!</div>;
-//   }
-// }
-
-// function App() {
-//   const name = "리액트";
-//   return (
-//     <>
-//       <div className="react">{name}</div>
-//       <input /> {/* self closing 태그 */}
-//     </>
-//   );
-// }
 
 export default App;
