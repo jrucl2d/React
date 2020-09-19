92187dd8c805228147f0d204d72e3d9dbf9f4a63
jrucl2d <ysk789465@gmail.com>
2020-09-19 13:22:45 +0900

    Using ref to Component studied

--- book/chapter2/src/App.js
+++ book/chapter2/src/App.js
@@ -1,9 +1,25 @@
-import React from "react";
-// import EventPractice_func from "./EventPractice_func";
-import ValidationSample from "./ValidationSample";
+// import React from "react";
+// // import EventPractice_func from "./EventPractice_func";
+// import ScrollBox from "./ScrollBox";
 
-const App = () => {
-  return <ValidationSample />;
-};
+// const App = () => {
+//   return <ScrollBox />;
+// };
 
+// export default App;
+
+import React, { Component } from "react";
+import ScrollBox from "./ScrollBox";
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <ScrollBox ref={(ref) => (this.scrollBox = ref)} />
+        <button onClick={() => this.scrollBox.scrollToBottom()}>
+          맨 밑으로
+        </button>
+      </div>
+    );
+  }
+}
 export default App;
