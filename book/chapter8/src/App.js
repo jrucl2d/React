a7e2fd3c7cf17498bda2efed146802c8209fe1fe
jrucl2d <ysk789465@gmail.com>
2020-09-21 20:04:18 +0900

    Studied useState

---
+++ book/chapter8/src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+// import logo from "./logo.svg";
+import "./App.css";
+// import Counter from "./Counter";
+import Info from "./Info";
+
+function App() {
+  // return <Counter />;
+  return <Info />;
+}
+
+export default App;
