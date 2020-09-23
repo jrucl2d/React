1fae02a2e88f3f7727b5a85d5ceb2430b132a4cc
jrucl2d <ysk789465@gmail.com>
2020-09-23 16:32:07 +0900

    todolist info data added

--- book/chapter10/src/components/TodoListItem.jsx
+++ book/chapter10/src/components/TodoListItem.jsx
@@ -4,14 +4,18 @@ import {
   MdCheckBox,
   MdRemoveCircleOutline,
 } from "react-icons/md";
+import cn from "classnames";
 import "./TodoListItem.scss";
 
-const TodoListItem = () => {
+const TodoListItem = ({ todo }) => {
+  const { text, checked } = todo;
+
   return (
     <div className="TodoListItem">
-      <div className="checkbox">
-        <MdCheckBoxOutlineBlank />
-        <div className="text">할일</div>
+      {/* checked가 true이면 checked라는 클래스도 추가되고 아니면 안 생긴다 */}
+      <div className={cn("checkbox", { checked })}>
+        {checked ? <MdCheckBox /> : <MdCheckBoxOutlineBlank />}
+        <div className="text">{text}</div>
       </div>
       <div className="remove">
         <MdRemoveCircleOutline />
