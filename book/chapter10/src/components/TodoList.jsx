5d80cba5a92063e0b41615dc42798be348592d59
jrucl2d <ysk789465@gmail.com>
2020-09-24 17:22:32 +0900

    Using react-virtualized, list optimazation done

--- book/chapter10/src/components/TodoList.jsx
+++ book/chapter10/src/components/TodoList.jsx
@@ -1,21 +1,35 @@
-import React from "react";
+import React, { useCallback } from "react";
+import { List } from "react-virtualized";
 import TodoListItem from "./TodoListItem";
 import "./TodoList.scss";
 
 const TodoList = ({ todos, onRemove, onToggle }) => {
+  const rowRenderer = useCallback(
+    ({ index, key, style }) => {
+      const todo = todos[index];
+      return (
+        <TodoListItem
+          todo={todo}
+          key={key}
+          onRemove={onRemove}
+          onToggle={onToggle}
+          style={style}
+        />
+      );
+    },
+    [onRemove, onToggle, todos]
+  );
   return (
-    <div className="TodoList">
-      {todos.map((todo) => {
-        return (
-          <TodoListItem
-            todo={todo}
-            key={todo.id}
-            onRemove={onRemove}
-            onToggle={onToggle}
-          />
-        );
-      })}
-    </div>
+    <List
+      className="TodoList"
+      width={512} // 전체 크기
+      height={513} // 전체 높이
+      rowCount={todos.length} // 항목 개수
+      rowHeight={56} // 항목 높이
+      rowRenderer={rowRenderer} // 항목 렌더링 때 쓰는 함수
+      list={todos} // 배열
+      style={{ outline: "none" }} // List에 기본적으로 적용되는 outline 스타일 제거
+    />
   );
 };
 
--- book/chapter10/src/components/TodoListItem.jsx
+++ book/chapter10/src/components/TodoListItem.jsx
@@ -7,21 +7,29 @@ import {
 import cn from "classnames";
 import "./TodoListItem.scss";
 
-const TodoListItem = ({ todo, onRemove, onToggle }) => {
+const TodoListItem = ({ todo, onRemove, onToggle, style }) => {
   const { id, text, checked } = todo;
 
   return (
-    <div className="TodoListItem">
-      {/* checked가 true이면 checked라는 클래스도 추가되고 아니면 안 생긴다 */}
-      <div className={cn("checkbox", { checked })} onClick={() => onToggle(id)}>
-        {checked ? <MdCheckBox /> : <MdCheckBoxOutlineBlank />}
-        <div className="text">{text}</div>
-      </div>
-      <div className="remove" onClick={() => onRemove(id)}>
-        <MdRemoveCircleOutline />
+    <div className="TodoListItem-virtualized" style={style}>
+      <div className="TodoListItem">
+        {/* checked가 true이면 checked라는 클래스도 추가되고 아니면 안 생긴다 */}
+        <div
+          className={cn("checkbox", { checked })}
+          onClick={() => onToggle(id)}
+        >
+          {checked ? <MdCheckBox /> : <MdCheckBoxOutlineBlank />}
+          <div className="text">{text}</div>
+        </div>
+        <div className="remove" onClick={() => onRemove(id)}>
+          <MdRemoveCircleOutline />
+        </div>
       </div>
     </div>
   );
 };
 
-export default React.memo(TodoListItem);
+export default React.memo(
+  TodoListItem,
+  (prevProps, nextProps) => prevProps.todo === nextProps.todo
+);
