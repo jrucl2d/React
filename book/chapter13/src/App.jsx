daea57cd29a6babdebaf408dce418340c0a75d05
jrucl2d <ysk789465@gmail.com>
2020-09-25 13:21:56 +0900

    Added Switch Routing system

--- book/chapter13/src/App.jsx
+++ book/chapter13/src/App.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { Route, Link } from "react-router-dom";
+import { Route, Link, Switch } from "react-router-dom";
 import About from "./About";
 import Home from "./Home";
 import Profiles from "./Profiles";
@@ -27,13 +27,24 @@ const App = () => {
         </li>
       </ul>
       <hr />
-      {/* exact를 true로 안 하면 /about을 쳐도 /에도 걸려서 /도 나타난다. */}
-      <Route path="/" component={Home} exact={true} />
-      {/* 다른 주소로 같은 라우터 접근 가능 */}
-      <Route path={["/about", "/info"]} component={About} />
-      {/* match.params를 통해서 username 정보를 가져올 수 있다.  */}
-      <Route path="/profiles" component={Profiles} />{" "}
-      <Route path="/history" component={HistorySample} />
+      <Switch>
+        {/* exact를 true로 안 하면 /about을 쳐도 /에도 걸려서 /도 나타난다. */}
+        <Route path="/" component={Home} exact={true} />
+        {/* 다른 주소로 같은 라우터 접근 가능 */}
+        <Route path={["/about", "/info"]} component={About} />
+        {/* match.params를 통해서 username 정보를 가져올 수 있다.  */}
+        <Route path="/profiles" component={Profiles} />{" "}
+        <Route path="/history" component={HistorySample} />
+      </Switch>
+      <Route
+        // path를 따로 설정하지 않으면 모든 상황에 렌더링됨
+        render={({ location }) => (
+          <div>
+            <h2>NOT FOUND</h2>
+            <p>{location.pathname}</p>
+          </div>
+        )}
+      />
     </>
   );
 };
