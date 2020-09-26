1848bab851c79f36d8c3db98341e1656dcfce439
jrucl2d <ysk789465@gmail.com>
2020-09-26 11:52:32 +0900

    Using axios get news articles and showing

--- book/chapter14/src/components/NewsList.jsx
+++ book/chapter14/src/components/NewsList.jsx
@@ -1,6 +1,7 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import styled from "styled-components";
 import NewsItem from "./NewsItem";
+import axios from "axios";
 
 const NewsListBlock = styled.div`
   box-sizing: border-box;
@@ -14,23 +15,41 @@ const NewsListBlock = styled.div`
     padding-right: 1rem;
   }
 `;
-
-const sampleArticle = {
-  title: "제목",
-  description: "내용",
-  url: "https://google.com",
-  urlToImage: "https://avatars2.githubusercontent.com/u/17202261?s=48&v=4",
-};
-
 const NewsList = () => {
+  const [articles, setArticles] = useState(null);
+  const [loading, setLoading] = useState(false);
+
+  useEffect(() => {
+    // useEffect 자체에 async를 사용하는 것은 불가.
+    const fetchData = async () => {
+      setLoading(true);
+      try {
+        const response = await axios.get(
+          "http://newsapi.org/v2/top-headlines?country=kr&apiKey=8b03715d8e07427093ea24fa793a2d64"
+        );
+        setArticles(response.data.articles);
+      } catch (err) {
+        console.error(err);
+      }
+      setLoading(false);
+    };
+    fetchData();
+  }, []);
+
+  if (loading) {
+    return <NewsListBlock>대기중...</NewsListBlock>;
+  }
+  // 아직 articles 값이 설정되지 않았을 때
+  // 이 작업이 없으면 아직 데이터가 없어서 null에는 map 함수가 없어서 렌더링 오류가 ㅂ라생함.
+  if (!articles) {
+    return null;
+  }
+
   return (
     <NewsListBlock>
-      <NewsItem article={sampleArticle} />
-      <NewsItem article={sampleArticle} />
-      <NewsItem article={sampleArticle} />
-      <NewsItem article={sampleArticle} />
-      <NewsItem article={sampleArticle} />
-      <NewsItem article={sampleArticle} />
+      {articles.map((article) => (
+        <NewsItem key={article.url} article={article} />
+      ))}
     </NewsListBlock>
   );
 };
