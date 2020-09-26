21be2203090ab7ba397d9c1ae936a3a766f28f75
jrucl2d <ysk789465@gmail.com>
2020-09-26 11:28:24 +0900

    Using axios

---
+++ book/chapter14/src/App.jsx
@@ -0,0 +1,32 @@
+import React, { useState } from "react";
+import axios from "axios";
+
+const App = () => {
+  const [data, setData] = useState(null);
+  const onClick = async () => {
+    try {
+      const response = await axios.get(
+        "https://jsonplaceholder.typicode.com/todos/1"
+      );
+      setData(response.data);
+    } catch (err) {
+      console.error(err);
+    }
+  };
+  return (
+    <>
+      <div>
+        <button onClick={onClick}>불러오기</button>
+      </div>
+      {data && (
+        <textarea
+          rows="7"
+          value={JSON.stringify(data, null, 2)}
+          readOnly="true"
+        />
+      )}
+    </>
+  );
+};
+
+export default App;
