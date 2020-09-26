ef308be2d4d73dfac8b1ddd64db8734bbb37267c
jrucl2d <ysk789465@gmail.com>
2020-09-26 12:54:10 +0900

    Using custom hooks to minify codes

--- book/chapter14/src/components/NewsList.jsx
+++ book/chapter14/src/components/NewsList.jsx
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from "react";
 import styled from "styled-components";
 import NewsItem from "./NewsItem";
 import axios from "axios";
+import usePromise from "../lib/usePromise";
 
 const NewsListBlock = styled.div`
   box-sizing: border-box;
@@ -16,36 +17,26 @@ const NewsListBlock = styled.div`
   }
 `;
 const NewsList = ({ category }) => {
-  const [articles, setArticles] = useState(null);
-  const [loading, setLoading] = useState(false);
-
-  useEffect(() => {
-    // useEffect 자체에 async를 사용하는 것은 불가.
-    const fetchData = async () => {
-      setLoading(true);
-      try {
-        const query = category === "all" ? "" : `&category=${category}`;
-        const response = await axios.get(
-          `http://newsapi.org/v2/top-headlines?country=kr${query}&apiKey=8b03715d8e07427093ea24fa793a2d64`
-        );
-        setArticles(response.data.articles);
-      } catch (err) {
-        console.error(err);
-      }
-      setLoading(false);
-    };
-    fetchData();
+  const [loading, response, error] = usePromise(() => {
+    const query = category === "all" ? "" : `&category=${category}`;
+    return axios.get(
+      `http://newsapi.org/v2/top-headlines?country=kr${query}&apiKey=8b03715d8e07427093ea24fa793a2d64`
+    );
   }, [category]);
 
   if (loading) {
     return <NewsListBlock>대기중...</NewsListBlock>;
   }
-  // 아직 articles 값이 설정되지 않았을 때
-  // 이 작업이 없으면 아직 데이터가 없어서 null에는 map 함수가 없어서 렌더링 오류가 ㅂ라생함.
-  if (!articles) {
+  // 아직 articles 값(response)이 설정되지 않았을 때
+  // 이 작업이 없으면 아직 데이터가 없어서 null에는 map 함수가 없어서 렌더링 오류가 발생함.
+  if (!response) {
     return null;
   }
+  if (error) {
+    return <NewsListBlock>에러 발생!</NewsListBlock>;
+  }
 
+  const { articles } = response.data;
   return (
     <NewsListBlock>
       {articles.map((article) => (
