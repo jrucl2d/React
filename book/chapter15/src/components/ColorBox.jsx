e5c7df5c1db96c03447f05baf131ea26f8affc63
jrucl2d <ysk789465@gmail.com>
2020-09-27 11:51:41 +0900

    Using class style context API

--- book/chapter15/src/components/ColorBox.jsx
+++ book/chapter15/src/components/ColorBox.jsx
@@ -1,29 +1,25 @@
-import React from "react";
-import { ColorConsumer } from "../contexts/color";
+import React, { useContext } from "react";
+import ColorContext from "../contexts/color";
 
 const ColorBox = () => {
+  const { state } = useContext(ColorContext);
   return (
-    <ColorConsumer>
-      {({ state }) => (
-        // children 자리에 함수를 넣어줌. Function as a child(혹은 Render Props)
-        <>
-          <div
-            style={{
-              width: "64px",
-              height: "64px",
-              background: state.color,
-            }}
-          />
-          <div
-            style={{
-              width: "32px",
-              height: "32px",
-              background: state.subcolor,
-            }}
-          />
-        </>
-      )}
-    </ColorConsumer>
+    <>
+      <div
+        style={{
+          width: "64px",
+          height: "64px",
+          background: state.color,
+        }}
+      />
+      <div
+        style={{
+          width: "32px",
+          height: "32px",
+          background: state.subcolor,
+        }}
+      />
+    </>
   );
 };
 
---
+++ book/chapter15/src/components/SelectColor_class.jsx
@@ -0,0 +1,43 @@
+import React, { Component } from "react";
+import ColorContext from "../contexts/color";
+
+const colors = ["red", "orange", "yellow", "green", "blue", "indigo", "violet"];
+
+class SelectColors extends Component {
+  static contextType = ColorContext; // this.context로 value에 바로 접근 가능
+
+  handleColor = (color) => {
+    this.context.actions.setColor(color);
+  };
+
+  handleSubcolor = (subcolor) => {
+    this.context.actions.setsubColor(subcolor);
+  };
+  render() {
+    return (
+      <div>
+        <h2>색상을 선택하세요.</h2>
+        <div style={{ display: "flex" }}>
+          {colors.map((color) => (
+            <div
+              key={color}
+              style={{
+                background: color,
+                width: "24px",
+                height: "24px",
+                cursor: "pointer",
+              }}
+              onClick={() => this.handleSetColor(color)}
+              onContextMenu={(e) => {
+                e.preventDefault();
+                this.handleSetSubcolor(color);
+              }}
+            />
+          ))}
+        </div>
+        <hr />
+      </div>
+    );
+  }
+}
+export default SelectColors;
