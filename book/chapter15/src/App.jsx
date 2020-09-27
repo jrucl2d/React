d86c7740b567f50952872625663a9425b9004d95
jrucl2d <ysk789465@gmail.com>
2020-09-27 11:41:09 +0900

    Added select color system

--- book/chapter15/src/App.jsx
+++ book/chapter15/src/App.jsx
@@ -1,12 +1,14 @@
 import React from "react";
 import ColorBox from "./components/ColorBox";
 import { ColorProvider } from "./contexts/color";
+import SelectColors from "./components/SelectColor";
 
 function App() {
   // Provider로 값을 제공해줄 수 있다. 기본값이 있어도 provider이 있으면 사용 안 함. Provider에 value 안 넣으면 오류남.
   return (
     <ColorProvider>
       <div>
+        <SelectColors />
         <ColorBox />
       </div>
     </ColorProvider>
