bebeeeb2f36f0531385c099c7c48b31e8247eae3
jrucl2d <ysk789465@gmail.com>
2020-09-27 11:16:36 +0900

    Using context API provider

--- book/chapter15/src/App.jsx
+++ book/chapter15/src/App.jsx
@@ -1,11 +1,15 @@
 import React from "react";
 import ColorBox from "./components/ColorBox";
+import ColorContext from "./contexts/color";
 
 function App() {
+  // Provider로 값을 제공해줄 수 있다. 기본값이 있어도 provider이 있으면 사용 안 함. Provider에 value 안 넣으면 오류남.
   return (
-    <div>
-      <ColorBox />
-    </div>
+    <ColorContext.Provider value={{ color: "red" }}>
+      <div>
+        <ColorBox />
+      </div>
+    </ColorContext.Provider>
   );
 }
 
