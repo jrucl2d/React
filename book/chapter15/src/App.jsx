9193db66e5f86826bfb363b2904bc3d10288376f
jrucl2d <ysk789465@gmail.com>
2020-09-27 11:25:53 +0900

    Update context API

--- book/chapter15/src/App.jsx
+++ book/chapter15/src/App.jsx
@@ -1,15 +1,15 @@
 import React from "react";
 import ColorBox from "./components/ColorBox";
-import ColorContext from "./contexts/color";
+import { ColorProvider } from "./contexts/color";
 
 function App() {
   // Provider로 값을 제공해줄 수 있다. 기본값이 있어도 provider이 있으면 사용 안 함. Provider에 value 안 넣으면 오류남.
   return (
-    <ColorContext.Provider value={{ color: "red" }}>
+    <ColorProvider>
       <div>
         <ColorBox />
       </div>
-    </ColorContext.Provider>
+    </ColorProvider>
   );
 }
 
--- book/chapter15/src/contexts/color.js
+++ book/chapter15/src/contexts/color.js
@@ -1,5 +1,29 @@
-import { createContext } from "react";
+import React, { createContext, useState } from "react";
 
-const ColorContext = createContext({ color: "black" }); // 기본상태 저장
+const ColorContext = createContext({
+  state: { color: "black", subcolor: "red" },
+  actions: {
+    setColor: () => {},
+    setSubcolor: () => {},
+  },
+}); // 기본상태 저장
+
+const ColorProvider = ({ children }) => {
+  const [color, setColor] = useState("black");
+  const [subcolor, setSubcolor] = useState("red");
+
+  const value = {
+    state: { color, subcolor },
+    action: { setColor, setSubcolor },
+  };
+  return (
+    <ColorContext.Provider value={value}>{children}</ColorContext.Provider>
+  );
+};
+
+// const ColorConsumer = ColorContext.Consumer과 같은 의미
+const { Consumer: ColorConsumer } = ColorContext;
+
+export { ColorProvider, ColorConsumer };
 
 export default ColorContext;
