07c194aef639199f86360b65a8130c38adf7eed6
jrucl2d <ysk789465@gmail.com>
2020-09-29 14:37:10 +0900

    Basic components made

---
+++ book/chapter17/src/components/Todos.jsx
@@ -0,0 +1,40 @@
+import React from "react";
+
+const TodoItem = ({ Todo, onToggle, onRemove }) => {
+  return (
+    <div>
+      <input type="checkbox" />
+      <span>예제 텍스트</span>
+      <button>삭제</button>
+    </div>
+  );
+};
+
+const Todos = ({
+  input,
+  todos,
+  onChangeInput,
+  onInsert,
+  onToggle,
+  onRemove,
+}) => {
+  const onSubmit = (e) => {
+    e.preventDefault();
+  };
+  return (
+    <div>
+      <form onSubmit={onSubmit}></form>
+      <input />
+      <button type="submit">등록</button>
+      <div>
+        <TodoItem />
+        <TodoItem />
+        <TodoItem />
+        <TodoItem />
+        <TodoItem />
+      </div>
+    </div>
+  );
+};
+
+export default Todos;
