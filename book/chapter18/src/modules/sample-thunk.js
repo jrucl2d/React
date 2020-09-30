d79b4f5507016ee06f366469f79482b89d5d3329
jrucl2d <ysk789465@gmail.com>
2020-09-30 12:09:57 +0900

    Axios get mothod with redux-saga

--- book/chapter18/src/modules/sample-thunk.js
+++ book/chapter18/src/modules/sample-thunk.js
@@ -1,8 +1,6 @@
-import { createAction, handleActions } from "redux-actions";
+import { handleActions } from "redux-actions";
 import * as api from "../lib/api";
 import createRequestThunk from "../lib/createRequestThunk";
-import { call, put, takeLatest } from "redux-saga/effects";
-import { startLoading, finishLoading } from "./loading";
 
 // 액션 타입 선언
 // 한 요청당 세 개를 만들어야 함
@@ -14,53 +12,55 @@ const GET_USERS = "sample/GET_USERS";
 const GET_USERS_SUCCESS = "sample/GET_USERS_SUCCESS";
 const GET_USERS_FAILURE = "sample/GET_USERS_FAILURE";
 
-export const getPost = createAction(GET_POST, (id) => id);
-export const getUsers = createAction(GET_USERS);
+// thunk 함수를 생성
+// thunk 함수 내부에서는 시작할 때 성공했을 때, 실패했을 때 다른 액션을 디스패치함
 
-function* getPostSaga(action) {
-  yield put(startLoading(GET_POST)); // 로딩 시작
-  // 파라미터로 action을 받아오면 액션의 정보를 조회 가능
-  try {
-    // call을 사용하면 Promise를 반환하는 함수를 호출하고 기다릴 수 있다.
-    // 첫 번째 파라미터는 함수, 나머지는 함수에 넣을 인수
-    const post = yield call(api.getPost, action.payload);
-    yield put({
-      type: GET_POST_SUCCESS,
-      payload: post.data,
-    });
-  } catch (err) {
-    yield put({
-      type: GET_POST_FAILURE,
-      payload: err,
-      error: true,
-    });
-  }
-  yield put(finishLoading(GET_POST));
-}
-function* getUsersSaga() {
-  yield put(startLoading(GET_USERS));
-  try {
-    const users = yield call(api.getUsers);
-    yield put({
-      type: GET_USERS_SUCCESS,
-      payload: users.data,
-    });
-  } catch (e) {
-    yield put({
-      type: GET_USERS_FAILURE,
-      payload: e,
-      error: true,
-    });
-  }
-  yield put(finishLoading(GET_USERS));
-}
+// export const getPost = (id) => async (dispatch) => {
+//   dispatch({ type: GET_POST }); // 요청 시작한 것을 알림
+//   try {
+//     const response = await api.getPost(id);
+//     dispatch({
+//       type: GET_POST_SUCCESS,
+//       payload: response.data,
+//     });
+//   } catch (err) {
+//     dispatch({
+//       type: GET_POST_FAILURE,
+//       payload: err,
+//       error: true,
+//     });
+//     throw err; // 나중에 컴포넌트단에서 에러를 조회할 수 있도록 해줌
+//   }
+// };
+export const getPost = createRequestThunk(GET_POST, api.getPost);
 
-export function* sampleSaga() {
-  yield takeLatest(GET_POST, getPostSaga);
-  yield takeLatest(GET_USERS, getUsersSaga);
-}
+// export const getUsers = () => async (dispatch) => {
+//   dispatch({ type: GET_USERS });
+//   try {
+//     const response = await api.getUsers();
+//     dispatch({
+//       type: GET_USERS_SUCCESS,
+//       payload: response.data,
+//     });
+//   } catch (err) {
+//     dispatch({
+//       type: GET_USERS_FAILURE,
+//       payload: err,
+//       error: true,
+//     });
+//     throw err;
+//   }
+// };
+export const getUsers = createRequestThunk(GET_USERS, api.getUsers);
+
+// 초기 상태 선언.
+// 요청의 로딩 중 상태는 loading이라는 객체에서 관리
 
 const initialState = {
+  // loading: {
+  //   GET_POST: false,
+  //   GET_USERS: false,
+  // },
   post: null,
   users: null,
 };
