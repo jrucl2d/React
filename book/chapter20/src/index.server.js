37434ff2396e5f2326d6c6de8fa32cb4d62db4f5
jrucl2d <ysk789465@gmail.com>
2020-10-02 15:26:48 +0900

    Server side rendering success

--- book/chapter20/src/index.server.js
+++ book/chapter20/src/index.server.js
@@ -1,7 +1,24 @@
 import React from "react";
 import ReactDOMServer from "react-dom/server";
+import express from "express";
+import { StaticRouter } from "react-router-dom";
+import App from "./App";
 
-const html = ReactDOMServer.renderToString(
-  <div>Hello Server Side Rendering</div>
-);
-console.log(html);
+const app = express();
+
+const serverRender = (req, res, next) => {
+  const context = {};
+  const jsx = (
+    <StaticRouter location={req.url} context={context}>
+      <App />
+    </StaticRouter>
+  );
+  const root = ReactDOMServer.renderToString(jsx); // 렌더링을 하고
+  res.send(root);
+};
+
+app.use(serverRender);
+
+app.listen(8000, () => {
+  console.log("server running on 8000");
+});
