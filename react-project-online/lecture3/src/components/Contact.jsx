64d420a4e3fbcb2623448bda4b4a35cc360baadc
jrucl2d <ysk789465@gmail.com>
2020-10-03 14:14:02 +0900

    Searching system done

--- react-project-online/lecture3/src/components/Contact.jsx
+++ react-project-online/lecture3/src/components/Contact.jsx
@@ -20,13 +20,29 @@ const Contact = () => {
       phone: "010-0000-0004",
     },
   ]);
+  const [keyword, setKeyword] = useState("");
   const mapToComponent = (data) => {
+    data.sort();
+    data = data.filter((contact) => {
+      return contact.name.toLowerCase().indexOf(keyword.toLowerCase()) > -1;
+    });
     return data.map((contact, i) => <ContactInfo contact={contact} key={i} />);
+  };
+
+  const onChange = (e) => {
+    setKeyword(e.target.value);
   };
 
   return (
     <div>
       <h1>Contacts</h1>
+      <input
+        type="text"
+        name="keyword"
+        placeholder="search"
+        value={keyword}
+        onChange={onChange}
+      />
       <div>{mapToComponent(contactData)}</div>
     </div>
   );
